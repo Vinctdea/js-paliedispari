b690d796f017da68b26e1b86bda4f0e0ca980b02
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-12 17:52:32 +0200

    feat: add all featur js and crate function FINE ESERCIZIO

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,9 +1,38 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
+// gestisco input user al click
+const verifica = document.querySelector("#button")
+
+// confrontare le parole con la funzione 
+verifica.addEventListener("click",
+    function(){
+        let parola = document.getElementById("parola");
+        let valoreParola = parola.value;
+        let risultato = VerificaPalindromi(valoreParola);
+        console.log(risultato);
+        
+        //  stampare il risultato
+        const result = document.querySelector("h2");
+        result.innerHTML = risultato;
+
+    }
+)
+
 
 
 
 // Creare una funzione per capire se la parola inserita è palindroma
+function VerificaPalindromi(input){
+    // Scomposizione della parola in singoli caratteri
+    let caratteri = input.split('');
+// Creazione di una versione invertita della parola
+    let invertita = caratteri.reverse().join('');
 
+// Verifica se la parola è palindroma
+    if (input === invertita) {
+	return input + " è una parola palindroma.";
+    } else {
+	return input + " non è una parola palindroma.";
+    }
 
-// confrontare le parole con la funzione e stampare il risultato
+}
