09e112d00821a189b6fed9fd12cd41cb1f0ed492
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-12 17:16:47 +0200

    build: add all files

---
+++ palindroma/js/main.js
@@ -0,0 +1,9 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+
+
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// confrontare le parole con la funzione e stampare il risultato
